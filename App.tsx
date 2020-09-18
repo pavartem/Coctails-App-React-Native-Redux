d5d534c1d306d68f0fe73327fd45fc0011d9c519
Artem Pavliuk <art256100@gmail.com>
2020-09-18 20:01:43 +0300

    Init project

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <View style={styles.body}>
+            <Text>test</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: '#ccc',
+  },
+  body: {
+    backgroundColor: '#fff',
+  },
+});
+
+export default App;
